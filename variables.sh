e99b829c12ffb70ed8f01f71c62b4a886b5e4236
venkatesh <venkateshvenky626@gmail.com>
2025-03-24 23:56:49 +0530

    add 2nd script

---
+++ variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=RAM
+PERSON2=SAI
+
+echo "$PERSON1: hello $PERSON2, good morning"
+echo "$PERSON2: Hi $PERSON1, very Good Morning"
+echo "$PERSON1: How are you $PERSON2?"
+echo "$PERSON2: I am good $PERSON1, How are you"
