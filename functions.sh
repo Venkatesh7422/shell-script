fe8117ca30067910e4c78a8d5315c3ee0a936f41
venkatesh <venkateshvenky626@gmail.com>
2025-03-25 21:13:15 +0530

    add 2nd script

--- functions.sh
+++ functions.sh
@@ -11,3 +11,18 @@ VALIDATE(){
         echo "$2 ... SUCCESS"
     fi
 }
+if [ $ID -ne 0 ]
+then
+    echo "ERROR:: Please run this script with root access"
+    exit 1 # you can give other than 0
+else
+    echo "You are root user"
+fi # fi means reverse of if, indicating condition end
+
+yum install mysql -y
+
+VALIDATE $? "Installing MySQL"
+
+yum install git -y
+
+VALIDATE $? "Installing GIT"
