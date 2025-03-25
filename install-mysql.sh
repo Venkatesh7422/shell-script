d2b8496e716bc7ad24e7acb09e52a5910a195cf1
venkatesh <venkateshvenky626@gmail.com>
2025-03-25 21:05:31 +0530

    add 2nd script

--- install-mysql.sh
+++ install-mysql.sh
@@ -15,7 +15,16 @@ yum install mysql -y
 if [$? -ne 0 ]
 then
     echo "ERROR:: INStalling mysql is failed"
-    exit2
+    exit1
 else
     echo "Installing MYSQL is Success"
+fi
+yum install git -y
+
+if [ $? -ne 0 ]
+then
+    echo "ERROR:: Installing GIT is failed"
+    exit 1
+else
+    echo "Installing GIT is SUCCESS"
 fi
