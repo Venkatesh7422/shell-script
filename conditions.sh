29ba85242cad7eab8cfef2fa813e15b3d1784f8b
venkatesh <venkateshvenky626@gmail.com>
2025-03-25 20:18:15 +0530

    add 2nd script

--- conditions.sh
+++ conditions.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 NUMBER=$1
+NUMBER1=$100
 
 if [ $NUMBER -ge 100 ]
 then
     echo "given number $NUMBER is greater than 100"
 else
     echo "given number $NUMBER is less than 100"
+else 
+    echo "giver number $Number1 IS EQUAL TO 100"
 fi
