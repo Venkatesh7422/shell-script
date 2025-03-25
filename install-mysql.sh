4b933feb33b19b572cbe1fe6cd632429ec2688ae
venkatesh <venkateshvenky626@gmail.com>
2025-03-25 21:04:04 +0530

    add 2nd script

--- install-mysql.sh
+++ install-mysql.sh
@@ -9,3 +9,13 @@ then
 else 
     echo "u r root user"
 fi
+
+yum install mysql -y
+
+if [$? -ne 0 ]
+then
+    echo "ERROR:: INStalling mysql is failed"
+    exit2
+else
+    echo "Installing MYSQL is Success"
+fi
