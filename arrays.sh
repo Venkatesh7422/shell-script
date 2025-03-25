48b53134db40c2ff08052967d071c7c18cbb2c1e
venkatesh <venkateshvenky626@gmail.com>
2025-03-25 20:07:17 +0530

    add 2nd script

--- arrays.sh
+++ arrays.sh
@@ -4,6 +4,6 @@ MOVIES=("SALAAR" "KALKI" "BAAHUBALI")
 
 echo "THIRD movie: ${MOVIES[2]}"
 echo "SECOND movie: ${MOVIES[1]}"
-echo "first movie: ${MOVIE[0]}"
+echo "first movie: ${MOVIEs[0]}"
 
 ECHO "ALL MOVIES: ${MOVIES[@]}"
