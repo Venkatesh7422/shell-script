402eb00a2c7df116828032133695e69b40b98a82
venkatesh <venkateshvenky626@gmail.com>
2025-03-26 18:28:28 +0530

    add 2nd script

--- colours.sh
+++ colours.sh
@@ -1,6 +1,36 @@
 #!/bin/bash
 
-echo -e "u r \e[31m root user\e"
+ID=$(id -u)
+TIMESTAMP=$(date +%F.%H.%M.%S )
 
-TIMESTAMP=$(date +%F-%H-%M-%S)
-echo "Script started executing at $TIMESTAMP"
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo 'Script started executing at $TIMESTAMP &>> $LOGFILE"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "ERROR:: $2 ... $R INSTALLATION FAILED $N"
+        exit 1
+    else
+        echo -e "$2 ... $G INSTALLATION SUCCESS $N "
+    fi       
+}
+
+if [ $ID -ne 0 ]
+then
+    echo -e "$R  ERROR:: RUN thus script with ROOT access $N"
+    exit 1
+else
+    echo -e "$G u r ROOT USER $N"
+fi
+
+yum install mysql -y &>> $LOGFILE
+VALIDATE $? "INSTALLING MYSQLD"
+
+yum install git -y &>> $LOGFILE
+VALIDATE $? "INSTALLING GIT"
