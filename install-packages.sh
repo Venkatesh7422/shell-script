a9e3a6a7cf1ddb4b287fe9204b4553d856db4b36
venkatesh <venkateshvenky626@gmail.com>
2025-03-26 19:59:58 +0530

    add 2nd script

--- install-packages.sh
+++ install-packages.sh
@@ -28,9 +28,9 @@ else
     echo "You are root user"
 fi # fi means reverse of if, indicating condition end
 
-echo "All arguments passed: $@"
-git mysql postfix net-tools
-package=git for first time
+#echo "All arguments passed: $@"
+#git mysql postfix net-tools
+#package=git for first time
 
 for package in $@
 do
