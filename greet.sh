495020a2ad3bb3726436aea41b9fd4a7a5bf2eae
venkatesh <venkateshvenky626@gmail.com>
2025-04-05 16:10:30 +0530

    2nd script

--- greet.sh
+++ greet.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
-person=$2
+HERO=""
 
-echo "hello $person, happy weekend"
+MOVIE_NAME=""
+
+USAGE(){
+    echo "USAGE:: $(basename $0) -h <hero> -m <movie name>"
+    echo "options::"
+    echo " -h, is hero name"
+    echo " -m, is movie name"
+    echo "-h, is help and exit"
+}
+
+While getopts ":h:m:h" opt; do
+    case $opt in
+        h) HERO="$optorg";;
+        m) MOVIE_NAME="$optorg";;
+        h|*) USAGE: exit;;
+    esac
+done
